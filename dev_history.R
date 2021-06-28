fffd5c1a6b75918fc54f841408dcf8828bcb6696
ROELANDT Nicolas <nicolas.roelandt@ifsttar.fr>
2021-06-28 10:22:52 +0200

    Configure pkgdown

--- dev_history.R
+++ dev_history.R
@@ -7,9 +7,9 @@ library(desc)
 unlink("DESCRIPTION")
 my_desc <- description$new("!new")
 my_desc$set_version("0.0.0.9000")
-my_desc$set(Package = "arabesqueFOSS4G2021")
-my_desc$set(Title = "Article prepared for the FOSS4G 2021 comparing Arabesque to other flow map applications in the geoweb")
-my_desc$set(Description = "Contains the article source plus data and code")
+my_desc$set(Package = "notesSIGR2021")
+my_desc$set(Title = "Notes from SIGR 2021 thematic school")
+my_desc$set(Description = "Contains a collection of notes files")
 my_desc$set("Authors@R",
             'c(
   person("Nicolas", "Roelandt", email = "nicolas.roelandt@univ-eiffel.fr", role = c("aut", "cre"), comment = c(ORCID = "0000-0001-9698-4275")),
@@ -29,7 +29,7 @@ usethis::use_readme_md()
 usethis::use_git()
 
 # Vignette
-usethis::use_vignette("article_FOSS4G2021")
+usethis::use_vignette("recherche_reproductible_Arnaud_Legrand")
 devtools::build_vignettes()
 
 # pkgdown and GH actions
@@ -39,9 +39,3 @@ usethis::use_build_ignore("_pkgdown.yml")
 
 # build website locally
 pkgdown::build_articles()
-
-# add function prepare_nodes
-usethis::use_package("sf")
-usethis::use_package("dplyr")
-usethis::use_r("prepare_nodes")
-usethis::use_test()
